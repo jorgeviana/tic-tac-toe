47fd53f45fd23dc8aba564ef63dd933f92cb7dae
Jorge Viana <jorgeviana@users.noreply.github.com>
2023-11-11 21:57:14 +0100

    move cells to gameEngine

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -15,15 +15,11 @@ function TicTacToe() {
     <>
       <h1 className="centered">Tic Tac Toe</h1>
       <div id="tic-tac-toe-board">
-        <div data-testid="NORTH_WEST" onClick={handleClick} className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div data-testid="WEST" onClick={handleClick} className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
+        { 
+          gameEngine.cells.map(
+            (cell) => <div data-testid={cell.id} onClick={handleClick} className="cell"></div>
+          )
+        }
       </div>
       <h3 data-testid="game-status" className="centered">{player} plays!</h3>
     </>
--- src/gameEngine.js
+++ src/gameEngine.js
@@ -6,5 +6,10 @@ const gameEngine = {
     nextPlayer: (player) => {
         if (player === 'X') return 'O';
         else return 'X';
-    }
+    },
+    cells: [
+        { id: 'NORTH_WEST' }, { id: 'NORTH'  }, { id: 'NORTH_EAST' },
+        { id: 'WEST'       }, { id: 'CENTER' }, { id: 'EAST'       },
+        { id: 'SOUTH_WEST' }, { id: 'SOUTH'  }, { id: 'SOUTH_EAST' }
+    ]
 }
