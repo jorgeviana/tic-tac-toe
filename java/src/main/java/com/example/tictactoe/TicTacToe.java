16528f5e08636e4185c1b94409e49708ca0316f9
Jorge Viana <jorge.viana@gft.com>
2024-04-09 19:44:31 +0200

    clean imports

--- java/src/main/java/com/example/tictactoe/TicTacToe.java
+++ java/src/main/java/com/example/tictactoe/TicTacToe.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static com.example.tictactoe.GameState.*;
 import static com.example.tictactoe.Player.*;
 
 public class TicTacToe {
@@ -13,14 +14,15 @@ public class TicTacToe {
     private Set<Move> moves = new HashSet<>();
 
     public GameState state() {
-        if (currentPlayer == null) return GameState.X_PLAYS;
-        if (currentPlayer == O) return GameState.X_PLAYS;
+        if (currentPlayer == null) return X_PLAYS;
+        if (currentPlayer == O) return X_PLAYS;
         return GameState.O_PLAYS;
     }
 
     public void play(Player player, Position position) {
         if (currentPlayer == player) return;
         if (playedPositions.contains(position)) return;
+
         currentPlayer = player;
         playedPositions.add(position);
         moves.add(new Move(player, position));
