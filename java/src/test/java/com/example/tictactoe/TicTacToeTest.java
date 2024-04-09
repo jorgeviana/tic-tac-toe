5a33457690d2a28555ee39014c52f7a7b469b65e
Jorge Viana <jorge.viana@gft.com>
2024-04-09 19:15:49 +0200

    test list

---
+++ java/src/test/java/com/example/tictactoe/TicTacToeTest.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe;
+
+public class TicTacToeTest {
+
+    // X_plays_first
+    // O_plays_after_X
+    // players_alternate
+    // cannot_play_occupied_position
+    // cannot_play_twice
+    // X_wins
+    // O_wins
+    // tie
+}
